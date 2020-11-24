dacba282eb46924cf4bbf22b2c2a572268dd43f4
Tran Phuoc Loc <giongnhuntlf@gmail.com>
2020-11-24 22:38:00 +0700

    ADD: cerateFooter + onload Header & Footer function

--- root/js/script.js
+++ root/js/script.js
@@ -34,3 +34,49 @@ function createHeaderNavbar() {
     html += '</nav>'
     body.insertAdjacentHTML("afterbegin", html)
 }
+
+function createFooter() {
+    var body = document.getElementsByTagName("body")[0];
+    var html = "";
+    html += '<footer class="page-footer bg-light font-small pt-4">'
+    html += '<div class="container-fluid text-center text-md-left">'
+    html += '<div class="row">'
+    html += '<div class="col-md-6 mt-md-0 mt-3">'
+    html += '<h5 class="text-uppercase">About Team</h5>'
+    html += '<p>Team with student members who are learning to use bs4 to create a website.</p>'
+    html += '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste quos, ratione cum tenetur perferendis et pariatur. Itaque veritatis vitae, eveniet delectus minus ut consequatur dolore enim quia odit atque sequi quidem maiores consequuntur provident quis assumenda ipsam. Odit deleniti fuga adipisci, velit sequi pariatur beatae ratione maxime, aperiam ad vel.</p>'
+    html += '</div>'
+    html += '<hr class="clearfix w-100 d-md-none pb-3">'
+    html += '<div class="col-md-3 mb-md-0 mb-3">'
+    html += '<h5 class="text-uppercase">Useful Links</h5>'
+    html += '<ul class="list-unstyled">'
+    html += '<li class="mb-3">'
+    html += '<a href="./index.html">Home</a>'
+    html += '</li>'
+    html += '<li class="mb-sm-3">'
+    html += '<a href="#!">About Us</a>'
+    html += '</li>'
+    html += '<li class="mb-sm-3">'
+    html += '<a href="#!">Help</a>'
+    html += '</li>'
+    html += '<li class="mb-sm-3">'
+    html += '<a href="#!">Your Account</a>'
+    html += '</li></ul></div>'
+    html += '<div class="col-md-3 mb-md-0 mb-3">'
+    html += '<h5 class="text-uppercase">Contacts</h5>'
+    html += '<ul class="list-unstyled"><li>'
+    html += '<p>Address: HCM, 123 HCM Street, VN</p>'
+    html += '</li><li><p>Phone: 0123456789</p>'
+    html += '</li><li><p>Social: <a href="#">fb.com/jafteam</a></p></li><li>'
+    html += '<p>E-Mail: jafteam@gmail.com</p>'
+    html += '</li></ul></div></div></div>'
+    html += '<div class="footer-copyright text-center text-white py-3" style="background-color: #dc3545;">© 2020 Copyright: '
+    html += '<a href="https://mdbootstrap.com/" style="color: white; text-decoration: underline;""> MDBootstrap.com</a>'
+    html += '</div>'
+    body.insertAdjacentHTML("beforeend", html)
+}
+
+function createHeaderFooter() {
+    createHeaderNavbar();
+    createFooter();
+}
