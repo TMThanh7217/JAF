71a6f6de53390aefaf7c2659859dbf14181573f3
Tran Phuoc Loc <giongnhuntlf@gmail.com>
2020-11-25 19:57:49 +0700

    ADD: menu creator

--- root/js/script.js
+++ root/js/script.js
@@ -1,5 +1,5 @@
 var MENU_PATH = "../json/menu.json"
-var json = '[{"id":0,"name":"Chili noodle soup with fresh vegetables on top","type":"food","src":"../images/food/Chili-noodle-soup-with-fresh-vegetables-on-top.jpg","price":10,"status":"stocking"},{"id":1,"name":"Full english breakfast","type":"food","src":"../images/food/full-english-breakfast.jpg","price":13,"status":"stocking"},{"id":2,"name":"Green salad with bread","type":"food","src":"../images/food/green-salad-with-bread.jpg","price":8,"status":"stocking"},{"id":3,"name":"Healthy green beans salad with egg and hemp seeds","type":"food","src":"../images/food/healthy-green-beans-salad-with-egg-and-hemp-seeds.jpg","price":9,"status":"stocking"},{"id":4,"name":"Juicy beef burger","type":"food","src":"../images/food/juicy-beef-burger.jpg","price":10,"status":"stocking"},{"id":5,"name":"omelet with ham cheese spinach and tomatoes","type":"food","src":"../images/food/omelet-with-ham-cheese-spinach-and-tomatoes.jpg","price":12,"status":"stocking"}]'
+var json = '[{"id":0,"name":"Noodle","detail":"Chili noodle soup with fresh vegetables on top","type":"food","src":"../images/food/Chili-noodle-soup-with-fresh-vegetables-on-top.jpg","price":10,"status":"stocking"},{"id":1,"name":"Breakfast Set","detail":"Full english breakfast","type":"food","src":"../images/food/full-english-breakfast.jpg","price":13,"status":"stocking"},{"id":2,"name":"Salad #1","detail":"Green salad with bread","type":"food","src":"../images/food/green-salad-with-bread.jpg","price":8,"status":"stocking"},{"id":3,"name":"Salad #2","detail":"Healthy green beans salad with egg and hemp seeds","type":"food","src":"../images/food/healthy-green-beans-salad-with-egg-and-hemp-seeds.jpg","price":9,"status":"stocking"},{"id":4,"name":"Beef Burger","detail":"Juicy beef burger","type":"food","src":"../images/food/juicy-beef-burger.jpg","price":10,"status":"stocking"},{"id":5,"name":"Omelet","detail":"omelet with ham cheese spinach and tomatoes","type":"food","src":"../images/food/omelet-with-ham-cheese-spinach-and-tomatoes.jpg","price":12,"status":"stocking"}]'
 
 function createHeaderNavbar() {
     var body = document.getElementsByTagName("body")[0];
@@ -87,14 +87,101 @@ function createHeaderFooter() {
 }
 
 function createTitle() {
-    var title = document.getElementsByTagName("title")[0];
+    let title = document.getElementsByTagName("title")[0];
     title.innerHTML = "JAF-Jerry And Friends";
-    var html = '<link rel="icon" href="../images/logo & bg & etc/JAF_logo.png">'
-    var head = document.getElementsByTagName("head")[0];
+    let html = '<link rel="icon" href="../images/logo & bg & etc/JAF_logo.png">'
+    let head = document.getElementsByTagName("head")[0];
     head.insertAdjacentHTML("afterbegin", html);
 }
 
-function loadMenu() {
-    var data = JSON.parse(json);
+function initMenu() {
+    let data = JSON.parse(json);
     console.log(data)
+    let menu = document.getElementById("menu");
+    console.log(menu);
+
+    let row1_products = data.slice(0, 3);
+    let row2_products = data.slice(3);
+
+    let row1 = createProductRow(row1_products);
+    let row2 = createProductRow(row2_products);
+
+    menu.appendChild(row1);
+    menu.appendChild(row2);
+}
+
+function createProductRow(product_array) {
+    let row = document.createElement("div");
+    row.className = "row card-group";
+    
+    let col_1 = createProductCol();
+    let col_1_card = createProductCard(product_array[0]);
+    col_1.appendChild(col_1_card);
+
+    let col_2 = createProductCol();
+    let col_2_card = createProductCard(product_array[1]);
+    col_2.appendChild(col_2_card);
+
+    let col_3 = createProductCol();
+    let col_3_card = createProductCard(product_array[2]);
+    col_3.appendChild(col_3_card);
+
+    row.appendChild(col_1);
+    row.appendChild(col_2);
+    row.appendChild(col_3);
+
+    return row;
+}
+
+function createProductCol() {
+    let col = document.createElement("div");
+    col.className = "col-md-4 col-lg-4 g-mb-30 tb-cell";
+    return col;
+}
+
+function createProductCard(p_data) {
+    // Card
+    let card = document.createElement("div");
+    card.className = "card u-shadow-v18 g-bg-white text-center rounded g-px-20 g-py-40 g-mb-5 h-100";
+    
+    // Card header
+    let card_header = document.createElement("div");
+    card_header.className = "card-header d-flex align-items-center justify-content-center h-100 g-bg-white";
+    
+    let card_img = document.createElement("img");
+    card_img.className = "img-thumbnail";
+    card_img.src = p_data.src;
+    card_img.alt = "";
+    card_header.appendChild(card_img);
+    
+    // Card body
+    let card_body = document.createElement("div");
+    card_body.className = "card-body d-flex flex-column";
+    
+    let card_body_header = document.createElement("h4");
+    card_body_header.className = "h5 g-color-black g-font-weight-600 g-mb-10";
+    card_body_header.innerHTML = p_data.name;
+    
+    let card_body_paragraph = document.createElement("p");
+    card_body_paragraph.innerHTML = p_data.detail;
+    
+    let card_body_span = document.createElement("span");
+    card_body_span.className = "d-block g-color-primary g-font-size-16";
+    card_body_span.innerHTML = p_data.price + "$";
+    
+    let card_body_button = document.createElement("button");
+    card_body_button.className = "btn btn-lg btn-block btn-order mt-3 mx-auto";
+    card_body_button.type = "button";
+    card_body_button.innerHTML = "Order Now";
+
+    card_body.appendChild(card_body_header);
+    card_body.appendChild(card_body_paragraph);
+    card_body.appendChild(card_body_span);
+    card_body.appendChild(card_body_button);
+
+    // Connect card header & card body
+    card.appendChild(card_header);
+    card.appendChild(card_body);
+
+    return card;
 }
