1f692f4465b489da68568a85636f39311cc3e5f8
Tran Phuoc Loc <giongnhuntlf@gmail.com>
2021-01-10 19:15:44 +0700

    finish cart feature

--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -4,9 +4,13 @@ var router = express.Router();
 router.get('/', (req, res) => {
     var cart = req.session.cart;
     res.locals.cart = cart.getCart();
+    let tax = (0.1 * cart.totalPrice);
+    let realPrice = (tax + cart.totalPrice);
     let page_data = {
         title: "JAF - Cart",
-        isEmpty: res.locals.totalQuantity == 0
+        isEmpty: res.locals.totalQuantity == 0,
+        tax: tax.toFixed(2),
+        realPrice: realPrice.toFixed(2)
     }
     res.render('cart', page_data);
 });
@@ -30,7 +34,15 @@ router.post('/remove', (req, res, next) => {
     var productID = Number(req.body.id);
     var cart = req.session.cart;
     cart.remove(productID);
-    res.json(cart.getCartItem(productID));
+    req.session.cart = cart;
+    let tax = (0.1 * cart.totalPrice);
+    let realPrice = (tax + cart.totalPrice);
+    let result = {};
+    result.totalPrice = cart.totalPrice.toFixed(2);
+    result.totalQuantity = cart.totalQuantity;
+    result.tax = tax.toFixed(2);
+    result.realPrice = realPrice.toFixed(2);
+    res.json(result);
 });
 
 module.exports = router;
