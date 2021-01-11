55e8443b2acd12344c4fd1c73853cbd6a23fa378
Tran Phuoc Loc <giongnhuntlf@gmail.com>
2021-01-11 21:42:13 +0700

    coupon create

--- controllers/couponController.js
+++ controllers/couponController.js
@@ -6,4 +6,8 @@ controller.getAll = async function() {
     return await Coupon.findAll({raw: true})
 }
 
+controller.create = async function(coupon) {
+    return await Coupon.create(coupon);
+}
+
 module.exports = controller
